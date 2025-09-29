2caef3b3714c21ae6b6ca61eee2af2a09ae95171
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-29 12:09:38 +0300

    feat: add di for mapper

--- WebAppDb/Program.cs
+++ WebAppDb/Program.cs
@@ -1,30 +1,44 @@
-using AutoMapper;
+
 using WebAppDb.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped(typeof(MapperConfig));
-    
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace WebAppDb
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //builder.Services.AddScoped(typeof(MapperConfig));
+
+            // Add services to the container.
+            // Replace this line:
+            // builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+            // With this line:
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
+            builder.Services.AddRazorPages();  
+            
+            
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
